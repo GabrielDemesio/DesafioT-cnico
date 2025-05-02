6fc62db6911e2614f3949e803d173acefa5b2ee6
Gabriel Demesio <cfmoneygamer@gmail.com>
2025-05-01 22:19:07 -0300

    criando endpoints

--- src/main/java/com/api/project/controller/EstatisticasController.java
+++ src/main/java/com/api/project/controller/EstatisticasController.java
@@ -4,6 +4,7 @@ import com.api.project.business.services.EstatisticasService;
 import com.api.project.business.services.TransacaoService;
 import com.api.project.controller.dtos.EstatisticasResponseDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/estatistica")
+@Slf4j
 public class EstatisticasController {
     private final TransacaoService transacaoService;
     private final EstatisticasService estatisticasService;
+
 
     public ResponseEntity<EstatisticasResponseDTO> buscarEstatisticas(
             @RequestParam(value = "intervaloBusca", required = false, defaultValue = "60") Integer intervaloBusca) {
